cc8f32e2182db44a32dc5be3a69570b1615367e5
Louxml <34997514+Louxml@users.noreply.github.com>
2018-03-23 21:04:01 +0800

    1.3.2
    前后端一部分衔接

--- js/政府预警1.js
+++ js/政府预警1.js
@@ -14,11 +14,31 @@ function headerInit(){
 headerInit();
 // 滚动事件结束
 
+// 导航条事件开始
+$('#nav').onePageNav({
+    currentClass: 'current',
+    changeHash: false,
+    scrollSpeed: 750,
+    scrollThreshold: 0.5,
+    filter: '',
+    easing: 'swing',
+    begin: function() {
+        //I get fired when the animation is starting
+    },
+    end: function() {
+        //I get fired when the animation is ending
+    },
+    scrollChange: function($currentListItem) {
+        //I get fired when you enter a section and I pass the list item of the section
+    }
+});
+// 导航条事件结束
+
 // 首页事件开始
 $('.index').backstretch([
-    "images/pot-holder.jpg",
-    "images/coffee.jpg",
-    "images/dome.jpg"
+    "../images/pot-holder.jpg",
+    "../images/coffee.jpg",
+    "../images/dome.jpg"
     ], {
       fade: 750,
       duration: 3000,
@@ -37,3 +57,5 @@ $('.filter li a').click(function(){
 })
 
 // 认识预警结束
+
+new WOW().init();
