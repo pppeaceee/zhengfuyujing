051e39c8de702f8038e8cb27585dcbeed4766681
godxu666666 <37233141+godxu666666@users.noreply.github.com>
2018-03-30 20:37:02 +0800

    4343
    4343

--- js/zheng.js
+++ js/zheng.js
@@ -74,3 +74,76 @@ $(window).resize(function () {
     $('.carousel-control').css('line-height', $height + 'px');
 })
 //轮播效果结束
+
+//登录注册开始
+window.onload=function(){
+    var oUserName=document.getElementById('username');
+
+}
+
+function checkUserName()
+    {
+         var oUserName = document.getElementById('username'); 
+         var oTest1 = document.getElementById('test1'); 
+         var pattern = /^[a-zA-Z0-9_]{4,15}$/;  //用户名格式正则表达式
+         if(oUserName.value.length == 0){ 
+            oTest1.innerHTML="用户名不能为空";
+            return false; 
+        } 
+        if(!pattern.test(oUserName.value)){ 
+            oTest1.innerHTML="用户名不合规范";       
+            return false; 
+        } 
+        else{ 
+            oTest1.innerHTML="OK" ;
+            return true; 
+        } 
+    }
+
+    function checkPassword()
+    {
+        var oPassword = document.getElementById('password'); 
+         var oTest2 = document.getElementById('test2'); 
+         var pattern = /^[a-zA-Z0-9_]{5,14}$/; 
+         if(!pattern.test(oPassword.value)){ 
+            oTest2.innerHTML="密码不合规范";
+           return false; 
+           } 
+          else{ 
+            oTest2.innerHTML="OK";
+            return true; 
+         } 
+    }
+
+    function ConfirmPassword()
+    {
+         var oConfirmPassword = document.getElementById('confirmpassword'); 
+         var oPassword = document.getElementById('password'); 
+         var oTest3 = document.getElementById('test3'); 
+         if((oConfirmPassword.value)!=(oPassword.value) || oConfirmPassword.value.length == 0){ 
+            oTest3.innerHTML="上下密码不一致"
+           return false; 
+           } 
+          else{ 
+            oTest3.innerHTML="OK"
+            return true; 
+          }  
+    }
+
+    function checkPhone()
+    {
+        var oPhoneNumber =document .getElementById('phonenumber');
+        var oTest4=document.getElementById('test4');
+        var pattern = /^1[34578]\d{9}$/; //验证手机号正则表达式 
+        if(!pattern.test(oPhoneNumber.value)){ 
+            oTest4.innerHTML="手机号码不合规范"
+           return false; 
+           } 
+          else{ 
+            oTest4.innerHTML="OK"
+            return true; 
+        } 
+    }
+
+
+//登录注册结束
